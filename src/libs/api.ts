5404e093995ad5ebfe2b762b294299047fe018a5
Guilherme Cheng <guilherme.cheng@gmail.com>
2022-05-16 01:07:58 -0300

    production server as baseURL

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://192.168.0.9:3333"
+    // baseURL: "http://192.168.0.9:3333" test local server
+    baseURL: "https://nlw-return-server-production-7be1.up.railway.app"
 })
